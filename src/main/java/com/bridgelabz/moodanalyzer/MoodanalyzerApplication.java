0e9105da1524c84e710c96e20d48c258f1d0455e
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-04 07:16:07 +0530

    Add: welcome message for mood analyzer program

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
